a7a40561483f0f14a7377aca298bc21cd26f3526
xingzhann <xingzhan@uw.edu>
2024-05-26 18:28:05 -0700

    hello

--- chart1_example.R
+++ chart1_example.R
@@ -10,5 +10,6 @@ colors <- c("Black" = "red", "White" = "green")
 ggplot(average_jails, aes(x = year)) +
   geom_bar(aes(y = average_black_jails, fill = "Black"), stat = "identity") +
   geom_bar(aes(y = average_white_jails, fill = "White"), stat = "identity") +
-  labs(x = "Year", y = "Average Jail Rate per 100K for US Counties", title = "Average Jailed (per 100k per year) for people by US County") +
+  labs(x = "Year", y = "Average Jail Rate per 100K for US Counties", 
+       title = "Average Jailed (per 100k per year) for people by US County") +
   scale_fill_manual(values = colors, name = "Race", labels = c("Black", "White"))
