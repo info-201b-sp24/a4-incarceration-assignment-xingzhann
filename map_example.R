5f6185731cb5c8b11363b09a4eed0473c4fd7fef
xingzhann <xingzhan@uw.edu>
2024-05-26 23:33:19 -0700

    done

--- map_example.R
+++ map_example.R
@@ -11,7 +11,7 @@ state_data <- data_2018 %>%
 
 plot_usmap(data = state_data, values = "mean_black_incarceration_rate", 
            labels = TRUE) +
-  scale_fill_continuous(name = "Incarcerations(Jailed) per 100k", label = scales::comma) +
+  scale_fill_continuous(name = "Incarcerations(Jailed) per 100k") +
   ggtitle("2018 Black Jail Rates per 100K(per state by state county mean)") +
   theme(legend.position = "right") +
   coord_sf()
